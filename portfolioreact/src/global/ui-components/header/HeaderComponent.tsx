de10880383e126bcc8b609463d9a5b7868dac4d9
acoss22 <anacseq@gmail.com>
2023-02-10 12:16:15 +0000

    style: header title

--- portfolioreact/src/global/ui-components/header/HeaderComponent.tsx
+++ portfolioreact/src/global/ui-components/header/HeaderComponent.tsx
@@ -1,10 +1,17 @@
-const HeaderComponent = () : any => {
+import styles from "./header.module.scss";
 
-    return(
-        <header>
-             Ana CSeq   
-        </header>
-    )
+const HeaderComponent = (): any => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+            <div className={styles.link}>
+              <span className={styles.title}>Ana CSeq</span>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderComponent;
