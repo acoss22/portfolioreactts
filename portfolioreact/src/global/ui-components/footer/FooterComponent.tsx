a6f0efedf7ecad7b86b144484167b7406cb8f3de
acoss22 <anacseq@gmail.com>
2023-02-10 12:46:43 +0000

    style: header and footer

--- portfolioreact/src/global/ui-components/footer/FooterComponent.tsx
+++ portfolioreact/src/global/ui-components/footer/FooterComponent.tsx
@@ -1,10 +1,13 @@
-const FooterComponent = () : any => {
+import styles from "./footer.module.scss";
 
-    return(
-        <footer>
-             @ 2023 Ana Sequeira. All rights reserved.   
-        </footer>
-    )
+const FooterComponent = (): any => {
+  return (
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        @ 2023 Ana Sequeira. All rights reserved.
+      </footer>
+    </div>
+  );
 };
 
 export default FooterComponent;
