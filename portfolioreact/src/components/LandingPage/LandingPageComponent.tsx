48247639af94ab40f62d130328804baa5920ab54
acoss22 <anacseq@gmail.com>
2023-02-12 13:12:07 +0000

    feat: add skillset

--- portfolioreact/src/components/LandingPage/LandingPageComponent.tsx
+++ portfolioreact/src/components/LandingPage/LandingPageComponent.tsx
@@ -24,9 +24,7 @@ const LandingPageComponent = (): any => {
         </div>
       </div>
 
-
-
-      <div className={styles.projects}>
+      <div className={styles.wrapper}>
         <span className={styles.projectsTitle}>Projects</span>
         <div className={styles.project}>
           <div className={styles.projectName}>Japanese Keyboard</div>
@@ -66,7 +64,7 @@ const LandingPageComponent = (): any => {
                 src={require("../../assets/img/imagesearch.png")}
               />
             </div>
-            <div className={styles.tags}>ssss#React #JavaScript</div>
+            <div className={styles.tags}>#React #JavaScript</div>
           </div>
           <div className={styles.project}>
             <div className={styles.projectName}>Old Portolio</div>
@@ -93,6 +91,71 @@ const LandingPageComponent = (): any => {
               />
             </div>
             <div className={styles.tags}>Angular #TypeScript</div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.wrapper}>
+        <span className={styles.projectsTitle}>Skillset</span>
+
+        <div className="row">
+          <div className="column">
+            <h2>Languages and Database</h2>
+            <h3>HTML</h3>
+            <h3>CSS</h3>
+            <h3>JavaScript</h3>
+            <h3>TypeScript</h3>
+            <h3>MySQL</h3>
+            <h3>MongoDB</h3>
+          </div>
+          <div className="column">
+            <h2>Frameworks and Libraries</h2>
+            <h3>Angular</h3>
+            <h3>React</h3>
+            <h3>Svelte</h3>
+            <h3>Mendix</h3>
+            <h3>NodeJS Express</h3>
+            <h3>JQuery</h3>
+            <h3>SASS</h3>
+            <h3>PWA</h3>
+            <h3>Ionic</h3>
+            <h3>Cordova</h3>
+            <h3>Redux</h3>
+          </div>
+          <div className="column">
+            <h2>Tools and Ways of Working</h2>
+            <h3>VSCode</h3>
+            <h3>Confluence</h3>
+            <h3>Bitbucket</h3>
+            <h3>JIRA</h3>
+            <h3>SCRUM/Agile</h3>
+            <h3>CI/CD</h3>
+            <h3>GIT</h3>
+            <h3>Slack</h3>
+            <h3>Jenkins</h3>
+            <h3>Docker</h3>
+            <h3>SonarQube</h3>
+            <h3>Figma</h3>
+            <h3>Maze</h3>
+            <h3>ARIS Enterprise</h3>
+            <h3>WebMethods</h3>
+          </div>
+          <div className="column">
+            <h2>Cloud</h2>
+            <h3>AWS S3</h3>
+            <h3>AWS CF</h3>
+            <h3>AWS Route53</h3>
+            <h3>AWS Registry</h3>
+            <h3>AWS Lambda</h3>
+            <h3>AWS API Gateway</h3>
+          </div>
+          <div className="column">
+            <h2>Testing</h2>
+            <h3>Enzyme</h3>
+            <h3>Jest</h3>
+            <h3>React Testing Library</h3>
+            <h3>Karma</h3>
+            <h3>Jasmine</h3>
           </div>
         </div>
       </div>
